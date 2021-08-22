491128288b749a13966ae0f2f0829c06585c60e1
Samuel Argueta <arg211024@uvg.edu.gt>
2021-08-22 00:43:30 -0600

    Nueva modificacion en la clase Familia

--- Familia.java
+++ Familia.java
@@ -3,8 +3,14 @@
  * Clase MODELO que da la funcion de las familias en la perrera.
  */
 public class Familia {
-    private String[][] familia;
-    private int i = 0;
+    private String[][] familias;
+    private String[][] menores = new String[12][3];
+    private String[][] mayores;
+    private String[][] ninguno;
+    private int i = 3;
+    private int j = 0;
+    private int x = 0;
+    private int z = 0;
 
     /**
      * Contructor que agrega los valore necesarios en las listas, segun sea
@@ -15,19 +21,22 @@ public class Familia {
      * @param ninios:      int
      */
     public Familia() {
-        familia = new String[15][3];
+        familias = new String[15][3];
+        menores = new String[12][3];
+        mayores = new String[12][3];
+        ninguno = new String[12][3];
         // Apellidos
-        familia[0][0] = "Argueta";
-        familia[1][0] = "Alburez";
-        familia[2][0] = "Mendoza";
+        familias[0][0] = "Argueta";
+        familias[1][0] = "Alburez";
+        familias[2][0] = "Mendoza";
         // integrantes
-        familia[0][1] = "2";
-        familia[1][1] = "4";
-        familia[2][1] = "3";
+        familias[0][1] = "2";
+        familias[1][1] = "4";
+        familias[2][1] = "3";
         // Cantidad de niños
-        familia[0][2] = "0";
-        familia[1][2] = "2";
-        familia[2][2] = "1";
+        familias[0][2] = "0";
+        familias[1][2] = "2";
+        familias[2][2] = "1";
     }
 
     /**
@@ -37,14 +46,14 @@ public class Familia {
         System.out.println("Apellidos       Cantidad de miembros    Cantidad de niños");
 
         // Ciclo para recorrer todos los reglones de la matriz
-        for (int i = 0; i < familia.length; i++) {
+        for (int i = 0; i < familias.length; i++) {
             // Cantidad de veces que se mostrar una respuesta . En este caso, solo se desea
             // mostrar 1 vez
             for (int j = 0; j < 1; j++) {
                 // Verificacion si hay null en una matriz, de haberlo, no se imprime;
-                if ((familia[i] != null) && (familia[i][j] != null)) {
-                    System.out.println(familia[i][0] + "                 " + familia[i][1] + "                      "
-                            + familia[i][2]);
+                if ((familias[i] != null) && (familias[i][j] != null)) {
+                    System.out.println(familias[i][0] + "                 " + familias[i][1] + "                      "
+                            + familias[i][2]);
                 } else {
                     continue;
                 }
@@ -57,30 +66,42 @@ public class Familia {
      * Setter que agrega la inforamcion de las familias con menores de 10 años
      */
     public void agregar_familia_menores(String apellido, String integrantes, String menores) {
-        familia[i][0] = apellido;
-        familia[i][1] = integrantes;
-        familia[i][2] = menores;
+        familias[i][0] = apellido;
+        familias[i][1] = integrantes;
+        familias[i][2] = menores;
         i++;
+        this.menores[j][0] = apellido;
+        this.menores[j][1] = integrantes;
+        this.menores[j][2] = menores;
+        j++;
     }
 
     /**
      * Setter que agrega la informacion de las familias con mayores de 10 años
      */
     public void agragar_familia_mayores(String apellido, String integrantes, String mayores) {
-        familia[i][0] = apellido;
-        familia[i][1] = integrantes;
-        familia[i][2] = mayores;
+        familias[i][0] = apellido;
+        familias[i][1] = integrantes;
+        familias[i][2] = mayores;
         i++;
+        this.mayores[x][0] = apellido;
+        this.mayores[x][1] = integrantes;
+        this.mayores[x][2] = mayores;
+        x++;
     }
 
     /**
-     * Setter que agrea la informacion de las familias que no tiene niños
+     * Setter que agrea la informacion de las familiass que no tiene niños
      */
     public void agregar_familia_ningun(String apellido, String integrantes, String ninguno) {
-        familia[i][0] = apellido;
-        familia[i][1] = integrantes;
-        familia[i][2] = ninguno;
+        familias[i][0] = apellido;
+        familias[i][1] = integrantes;
+        familias[i][2] = ninguno;
         i++;
+        this.ninguno[z][0] = apellido;
+        this.ninguno[z][0] = integrantes;
+        this.ninguno[z][0] = ninguno;
+        z++;
     }
 
 }
